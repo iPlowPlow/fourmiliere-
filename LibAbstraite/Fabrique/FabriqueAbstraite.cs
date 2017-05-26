9185cdeb77538e2eb2824e076237a86df6c9bcc1
Pierre lochouarn <Pierre lochouarn>
2017-05-26 18:03:49 +0200

    env

--- LibAbstraite/Fabrique/FabriqueAbstraite.cs
+++ LibAbstraite/Fabrique/FabriqueAbstraite.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fourmilliereALIHM
+namespace LibAbstraite.Fabrique
 {
     public abstract class FabriqueAbstraite
     {
