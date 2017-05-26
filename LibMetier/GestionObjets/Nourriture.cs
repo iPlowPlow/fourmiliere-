d463332dd2917791fdcd81f4a6684252f995e4da
iPlowPlow <goasguen.loic@gmail.com>
2017-05-26 16:25:24 +0200

    Création de la structure libMetier

---
+++ LibMetier/GestionObjets/Nourriture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMetier.GestionObjets
+{
+    class Nourriture : ObjetAbstract
+    {
+        String nom { get; set; }
+
+        public Nourriture(String nom)
+        {
+            this.nom = nom;
+        }
+
+    }
+}
---
+++ LibMetier/GestionObjets/Pheromone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMetier.GestionObjets
+{
+    class Pheromone : ObjetAbstract
+    {
+        String nom { get; set; }
+
+        public Pheromone(String nom)
+        {
+            this.nom = nom;
+        }
+
+    }
+}
