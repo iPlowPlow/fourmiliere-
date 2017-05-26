e48e1494b156a90d2ce69d34346faf879d708bc0
Pierre lochouarn <Pierre lochouarn>
2017-05-26 15:01:50 +0200

    first

---
+++ fourmilliereALIHM/AProposWindows.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fourmilliereALIHM
+{
+    /// <summary>
+    /// Logique d'interaction pour AProposWindows.xaml
+    /// </summary>
+    public partial class AProposWindows : Window
+    {
+        public AProposWindows()
+        {
+            InitializeComponent();
+            DataContext = new AproposViewModel();
+        }
+    }
+}
